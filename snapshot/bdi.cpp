0168c3c216b648651b5139d16c43708a403dfbd5
Zhewen <zhewen.pan@wisc.edu>
2022-11-09 14:14:28 -0600

    xor bdi

--- snapshot/bdi.cpp
+++ snapshot/bdi.cpp
@@ -18,6 +18,8 @@ struct Line {
    bool v8;
    bool v4;
    bool v2;
+   bool cachedAbove;
+   bool xored;
 
    unsigned size;
    unsigned b8d1;
@@ -70,8 +72,8 @@ uint64_t * read_tag(unsigned *size, FILE * file) //Don't forget to free retval a
    return retval;
 }
 
-void bdi(struct Line* line_array, unsigned lineSize, u_int8_t * p, unsigned numLine) {
-   printf("calling %s\n", __func__);
+void init_line_data(struct Line* line_array, unsigned lineSize, u_int8_t * p, unsigned numLine) {
+   // printf("calling %s\n", __func__);
    // for every line
    for(unsigned i = 0; i < numLine; i++) {
       line_array[i].b8d1 = lineSize;
@@ -124,6 +126,17 @@ void bdi(struct Line* line_array, unsigned lineSize, u_int8_t * p, unsigned numL
          }
          line_array[i].segs2[js] = p_seg;
          // printf("%" PRIu16 "\n", line_array[i].segs2[js]);
+      }
+   }
+}
+
+void bdi(struct Line* line_array, unsigned lineSize, u_int8_t * p, unsigned numLine, unsigned do_xor) {
+   // printf("calling %s\n", __func__);
+   // for every line
+   for(unsigned i = 0; i < numLine; i++) {
+      if (do_xor != 0 && line_array[i].cachedAbove) {
+         line_array[i].size = 0;
+         continue;
       }
 
       // try compression
@@ -377,313 +390,134 @@ void bdi(struct Line* line_array, unsigned lineSize, u_int8_t * p, unsigned numL
    }
 }
 
-void bdi_xor(struct Line* line_array, unsigned lineSize, 
+unsigned countSetBits(uint64_t n)
+{
+    unsigned count = 0;
+    while (n != 0) {
+        count += n & 1;
+        n >>= 1;
+    }
+    return count;
+}
+
+void xor_preprocess(struct Line* line_array, unsigned lineSize, 
    u_int8_t * p, unsigned numLine, 
-   uint64_t * p_l1i_tag, uint64_t * p_l1d_tag,
-   uint64_t * p_l2_tag) {
-   printf("calling %s\n", __func__);
-   // for every line
+   uint64_t * p_l1i_tag, unsigned numLine_l1i,
+   uint64_t * p_l1d_tag, unsigned numLine_l1d,
+   uint64_t * p_l2_tag, unsigned do_xor) {
+   // printf("calling %s\n", __func__);
+   // first pass: for every line
+   // 1. find if exists in L1
+   unsigned ct = 0;
    for(unsigned i = 0; i < numLine; i++) {
-      line_array[i].b8d1 = lineSize;
-      line_array[i].b4d1 = lineSize;
-      line_array[i].b8d2 = lineSize;
-      line_array[i].b2d1 = lineSize;
-      line_array[i].b4d2 = lineSize;
-      line_array[i].b8d4 = lineSize;
-      line_array[i].zero = lineSize;
-      line_array[i].rep = lineSize;
-      line_array[i].size = lineSize;
-      line_array[i].v8 = false;
-      line_array[i].v4 = false;
-      line_array[i].v2 = false;
-      // printf("line#%d: ", i);
-
-      //create segments
-      unsigned base=8;
-      line_array[i].segs8 = (uint64_t *) malloc(lineSize/base * sizeof(uint64_t));
-      for (unsigned js = 0; js < lineSize/base; js++) {
-         u_int64_t p_seg = 0; 
-         for (unsigned j = 0; j < base; j++) {
-            p_seg = p_seg << base;
-            p_seg += p[i*lineSize + js *base + j];
-         }
-         line_array[i].segs8[js] = p_seg;
-         // printf("%"PRIu64" ", line_array[i].segs8[js]);
-      }
-      // printf("\n");
-
-      base=4;
-      line_array[i].segs4 = (uint32_t *) malloc(lineSize/base * sizeof(uint32_t));
-      for (unsigned js = 0; js < lineSize/base; js++) {
-         u_int32_t p_seg = 0; 
-         for (unsigned j = 0; j < base; j++) {
-            p_seg = p_seg << base;
-            p_seg += p[i*lineSize + js *base + j];
-         }
-         line_array[i].segs4[js] = p_seg;
-         // printf("%" PRIu32 "\n", line_array[i].segs4[js]);
-      }
-
-      base=2;
-      line_array[i].segs2 = (uint16_t *) malloc(lineSize/base * sizeof(uint16_t));
-      for (unsigned js = 0; js < lineSize/base; js++) {
-         u_int16_t p_seg = 0; 
-         for (unsigned j = 0; j < base; j++) {
-            p_seg = p_seg << base;
-            p_seg += p[i*lineSize + js *base + j];
-         }
-         line_array[i].segs2[js] = p_seg;
-         // printf("%" PRIu16 "\n", line_array[i].segs2[js]);
-      }
-
-      // try compression
-      // zero
-      line_array[i].zero = 1;
-      for (unsigned j=0; j < lineSize/8; j++) {
-         if(line_array[i].segs8[j] != UINT64_C(0)) {
-            line_array[i].zero = lineSize;
-            // printf("i=%d, j=%d: nonzero detected, ls=%d\n", i, j,line_array[i].zero);
+      line_array[i].cachedAbove = false;
+      line_array[i].xored = false;
+      // read l2 tag
+      uint64_t l2_addr = p_l2_tag[i];
+      // search in l1i
+      for (unsigned l1i = 0; l1i < numLine_l1i; l1i++) {
+         if(l2_addr == p_l1i_tag[l1i]) {
+            line_array[i].cachedAbove = true;
+            ct++;
+            // printf("l2 line is cached in l1i!\n");
             break;
          }
       }
-      if (line_array[i].zero != lineSize) {
-         line_array[i].size = line_array[i].zero;
-         continue;
-      }
-
-      // rep
-      line_array[i].rep = 8;
-      uint64_t ref = 0;
-      for (unsigned j=0; j < lineSize/8; j++) {
-         if(j == 0) {
-            ref = line_array[i].segs8[j];
-         }
-         else {
-            if (line_array[i].segs8[j] != ref) {
-               line_array[i].rep = lineSize;
+      // search in l1d
+      if (!line_array[i].cachedAbove) {
+         for (unsigned l1d = 0; l1d < numLine_l1d; l1d++) {
+            if(l2_addr == p_l1d_tag[l1d]) {
+               line_array[i].cachedAbove = true;
+               ct++;
+               // printf("l2 line is cached in l1d!\n");
                break;
             }
          }
       }
-      if (line_array[i].rep != lineSize) {
-         line_array[i].size = line_array[i].rep;
-         continue;
-      }
+   }
+   printf("ct=%d\n", ct);
+   //cached above lines are too many!
+   assert(ct <= numLine/2);
 
-      // b8d1
-      line_array[i].b8d1 = 0;
-      line_array[i].b8d1 += 8;
-      for (unsigned j=0; j < lineSize/8; j++) {
-         if(line_array[i].segs8[j] <= UINT8_MAX) {
-            line_array[i].b8d1 += 1;
+   // second pass: for every line cached above
+   // 1. xor with another block and modify data
+   for(unsigned i = 0; i < numLine; i++) {
+      // perform xor
+      if (line_array[i].cachedAbove) {
+         unsigned ind;
+         if (do_xor == 1) {
+            // rand xor
+            // printf("using rand xor\n");
+            do {
+               ind = rand() % numLine;
+            } while(ind != i && 
+               line_array[ind].cachedAbove == false && 
+               line_array[ind].xored == false);
+            line_array[ind].xored = true;
+            line_array[i].xored = true;
+            // printf("[%d] ^ [%d]\n", i, ind);
+
+            for (unsigned js = 0; js < lineSize/8; js++) {
+               uint64_t temp8 = line_array[ind].segs8[js] ^ line_array[i].segs8[js];
+               line_array[ind].segs8[js] = temp8;
+               line_array[i].segs8[js] = temp8;
+               // printf("%" PRIu32 "\n", temp8);
+            }
+            for (unsigned js = 0; js < lineSize/4; js++) {
+               uint32_t temp4 = line_array[ind].segs4[js] ^ line_array[i].segs4[js];
+               line_array[ind].segs4[js] = temp4;
+               line_array[i].segs4[js] = temp4;
+            }
+            for (unsigned js = 0; js < lineSize/2; js++) {
+               uint16_t temp2 = line_array[ind].segs2[js] ^ line_array[i].segs2[js];
+               line_array[ind].segs2[js] = temp2;
+               line_array[i].segs2[js] = temp2;
+            }
+            
+         }
+         else if (do_xor == 2) {
+            // printf("using ideal xor\n");
+            // ideal xor
+            unsigned min_ham = lineSize*8 + 1;
+            unsigned best_cand_ind = numLine + 1;
+            for (unsigned ind = 0; ind < numLine; ind++) {
+               unsigned ham = 0;
+               for (unsigned js = 0; js < lineSize/8; js++) {
+                  uint64_t temp8 = line_array[ind].segs8[js] ^ line_array[i].segs8[js];
+                  ham += countSetBits(temp8);
+               }
+               if (ham < min_ham && 
+                  line_array[ind].cachedAbove == false &&
+                  line_array[ind].xored == false) {
+                  min_ham = ham;
+                  best_cand_ind = ind;
+               }               
+            }
+            // printf("min ham[%d]=%d\n", best_cand_ind, min_ham);
+            
+            for (unsigned js = 0; js < lineSize/8; js++) {
+               uint64_t temp8 = line_array[best_cand_ind].segs8[js] ^ line_array[i].segs8[js];
+               line_array[best_cand_ind].segs8[js] = temp8;
+               line_array[i].segs8[js] = temp8;
+               // printf("%" PRIu32 "\n", temp8);
+            }
+            for (unsigned js = 0; js < lineSize/4; js++) {
+               uint32_t temp4 = line_array[best_cand_ind].segs4[js] ^ line_array[i].segs4[js];
+               line_array[best_cand_ind].segs4[js] = temp4;
+               line_array[i].segs4[js] = temp4;
+            }
+            for (unsigned js = 0; js < lineSize/2; js++) {
+               uint16_t temp2 = line_array[best_cand_ind].segs2[js] ^ line_array[i].segs2[js];
+               line_array[best_cand_ind].segs2[js] = temp2;
+               line_array[i].segs2[js] = temp2;
+            }
+            line_array[best_cand_ind].xored = true;
+            line_array[i].xored = true;
          }
          else {
-            if (line_array[i].v8 == false) {
-               line_array[i].v8 = true;
-               line_array[i].base8 = line_array[i].segs8[j];
-            }
-            assert(line_array[i].v8 == true);
-            if (((line_array[i].segs8[j] >= line_array[i].base8)?
-               (line_array[i].segs8[j] - line_array[i].base8) : 
-               (line_array[i].base8 - line_array[i].segs8[j])) <= UINT8_MAX) {
-               line_array[i].b8d1 += 1;
-            }
-            else {
-               line_array[i].b8d1 = lineSize;
-               break;
-            }
+            assert(false);
          }
       }
-      line_array[i].v8 = false;
-      if (line_array[i].b8d1 != lineSize) {
-         line_array[i].size = line_array[i].b8d1;
-         continue;
-      }
-      
-      // b4d1
-      line_array[i].b4d1 = 0;
-      if (line_array[i].b8d1 != lineSize) {line_array[i].b4d1 = lineSize;}
-      else {
-         line_array[i].b4d1 += 4;
-         for (unsigned j=0; j < lineSize/4; j++) {
-            if(line_array[i].segs4[j] <= UINT8_MAX) {
-               line_array[i].b4d1 += 1;
-            }
-            else {
-               if (line_array[i].v4 == false) {
-                  line_array[i].v4 = true;
-                  line_array[i].base4 = line_array[i].segs4[j];
-               }
-               assert(line_array[i].v4 == true);
-               if (((line_array[i].segs4[j] >= line_array[i].base4)?
-               (line_array[i].segs4[j] - line_array[i].base4) : 
-               (line_array[i].base4 - line_array[i].segs4[j])) <= UINT8_MAX) {
-                  line_array[i].b4d1 += 1;
-               }
-               else {
-                  line_array[i].b4d1 = lineSize;
-                  break;
-               }
-            }
-         }
-      }
-      line_array[i].v4 = false;
-      if (line_array[i].b4d1 != lineSize) {
-         line_array[i].size = line_array[i].b4d1;
-         continue;
-      }
-      
-      // b8d2
-      line_array[i].b8d2 = 0;
-      if (line_array[i].b8d1 != lineSize || 
-         line_array[i].b4d1 != lineSize) {
-            line_array[i].b8d2 = lineSize;
-      }
-      else {
-         line_array[i].b8d2 += 8;
-         for (unsigned j=0; j < lineSize/8; j++) {
-            if(line_array[i].segs8[j] <= UINT16_MAX) {
-               line_array[i].b8d2 += 2;
-            }
-            else {
-               if (line_array[i].v8 == false) {
-                  line_array[i].v8 = true;
-                  line_array[i].base8 = line_array[i].segs8[j];
-               }
-               assert(line_array[i].v8 == true);
-               if (((line_array[i].segs8[j] >= line_array[i].base8)?
-               (line_array[i].segs8[j] - line_array[i].base8) : 
-               (line_array[i].base8 - line_array[i].segs8[j])) <= UINT16_MAX) {
-                  line_array[i].b8d2 += 2;
-               }
-               else {
-                  line_array[i].b8d2 = lineSize;
-                  break;
-               }
-            }
-         }
-      }
-      line_array[i].v8 = false;
-      if (line_array[i].b8d2 != lineSize) {
-         line_array[i].size = line_array[i].b8d2;
-         continue;
-      }
-
-      // b2d1
-      line_array[i].b2d1 = 0;
-      if (line_array[i].b8d1 != lineSize || 
-         line_array[i].b4d1 != lineSize || 
-         line_array[i].b8d2 != lineSize) {
-            line_array[i].b2d1 = lineSize;
-      }
-      else {
-         line_array[i].b2d1 += 2;
-         for (unsigned j=0; j < lineSize/2; j++) {
-            if(line_array[i].segs2[j] <= UINT8_MAX) {
-               line_array[i].b2d1 += 1;
-            }
-            else {
-               if (line_array[i].v2 == false) {
-                  line_array[i].v2 = true;
-                  line_array[i].base2 = line_array[i].segs2[j];
-               }
-               assert(line_array[i].v2 == true);
-               if (((line_array[i].segs2[j] >= line_array[i].base2)?
-               (line_array[i].segs2[j] - line_array[i].base2) : 
-               (line_array[i].base2 - line_array[i].segs2[j])) <= UINT8_MAX) {
-                  line_array[i].b2d1 += 1;
-               }
-               else {
-                  line_array[i].b2d1 = lineSize;
-                  break;
-               }
-            }
-         }
-      }
-      line_array[i].v2 = false;
-      if (line_array[i].b2d1 != lineSize) {
-         line_array[i].size = line_array[i].b2d1;
-         continue;
-      }
-
-      // b4d2
-      line_array[i].b4d2 = 0;
-      if (line_array[i].b8d1 != lineSize || 
-         line_array[i].b4d1 != lineSize || 
-         line_array[i].b8d2 != lineSize || 
-         line_array[i].b2d1 != lineSize) {
-            line_array[i].b4d2 = lineSize;
-      }
-      else {
-         line_array[i].b4d2 += 4;
-         for (unsigned j=0; j < lineSize/4; j++) {
-            if(line_array[i].segs4[j] <= UINT16_MAX) {
-               line_array[i].b4d2 += 2;
-            }
-            else {
-               if (line_array[i].v4 == false) {
-                  line_array[i].v4 = true;
-                  line_array[i].base4 = line_array[i].segs4[j];
-               }
-               assert(line_array[i].v4 == true);
-               if (((line_array[i].segs4[j] >= line_array[i].base4)?
-               (line_array[i].segs4[j] - line_array[i].base4) : 
-               (line_array[i].base4 - line_array[i].segs4[j])) <= UINT16_MAX) {
-                  line_array[i].b4d2 += 2;
-               }
-               else {
-                  line_array[i].b4d2 = lineSize;
-                  break;
-               }
-            }
-         }
-      }
-      line_array[i].v4 = false;
-      if (line_array[i].b4d2 != lineSize) {
-         line_array[i].size = line_array[i].b4d2;
-         continue;
-      }
-
-      // b8d4
-      line_array[i].b8d4 = 0;
-      if (line_array[i].b8d1 != lineSize || 
-         line_array[i].b4d1 != lineSize || 
-         line_array[i].b8d2 != lineSize || 
-         line_array[i].b2d1 != lineSize || 
-         line_array[i].b4d2 != lineSize) {
-            line_array[i].b8d4 = lineSize;
-      }
-      else {
-         line_array[i].b8d4 += 8;
-         for (unsigned j=0; j < lineSize/8; j++) {
-            if(line_array[i].segs8[j] <= UINT32_MAX) {
-               line_array[i].b8d4 += 4;
-            }
-            else {
-               if (line_array[i].v8 == false) {
-                  line_array[i].v8 = true;
-                  line_array[i].base8 = line_array[i].segs8[j];
-               }
-               assert(line_array[i].v8 == true);
-               if (((line_array[i].segs8[j] >= line_array[i].base8)?
-               (line_array[i].segs8[j] - line_array[i].base8) : 
-               (line_array[i].base8 - line_array[i].segs8[j])) <= UINT32_MAX) {
-                  line_array[i].b8d4 += 4;
-               }
-               else {
-                  line_array[i].b8d4 = lineSize;
-                  break;
-               }
-            }
-         }
-      }
-      line_array[i].v8 = false;
-      if (line_array[i].b8d4 != lineSize) {
-         line_array[i].size = line_array[i].b8d4;
-         continue;
-      }
-
-      
    }
 }
 
@@ -692,8 +526,22 @@ int main(int argc, char *argv[]) {
       printf("./bdi dir do_xor\n");
       return 1;
    }
-   bool do_xor = (!strcmp(argv[2],"1"))? true : false;
-   // printf("doxor=%d\n", do_xor);
+
+   unsigned do_xor;
+   if (!strcmp(argv[2], "rand")) {
+      do_xor = 1;
+   }
+   else if (!strcmp(argv[2], "ideal")) {
+      do_xor = 2;
+   }
+   else if (!strcmp(argv[2], "none")) {
+      do_xor = 0;
+   }
+   else {
+      printf("unknow doxor option: %s\n", argv[2]);
+      return 1;
+   }
+
    char filename[256];
    char filename_l1i[256];
    char filename_l1d[256];
@@ -723,12 +571,16 @@ int main(int argc, char *argv[]) {
    unsigned numLine = numByte/lineSize;
    printf("numLine=%d, lineSize=%d\n", numLine, lineSize);
    fclose(fp);
+   if (numLine == 0) {
+      printf("no valid line in L2!\n");
+      return 1;
+   }
    struct Line* line_array = (Line*) malloc(numLine * sizeof(struct Line));
    
    uint64_t * p_l2_tag;
    uint64_t * p_l1i_tag;
    uint64_t * p_l1d_tag;
-   if (do_xor == true) {
+   if (do_xor == 1 || do_xor == 2) {
       // read l2 tag
       FILE *fp_l2_tag;
       fp_l2_tag = fopen(filename_l2, "r");
@@ -739,8 +591,9 @@ int main(int argc, char *argv[]) {
       unsigned size_l2_tag = 0;
       p_l2_tag = read_tag(&size_l2_tag, fp_l2_tag);
       fclose(fp_l2_tag);
-      for (unsigned i = 0; i < size_l2_tag; i++) {
-         printf("l2 tag [%d]%lx\n", i, p_l2_tag[i]);
+      if (size_l2_tag == 0) {
+         printf("no valid addr in L2!\n");
+         return 1;
       }
 
       // read l1d tag
@@ -753,8 +606,9 @@ int main(int argc, char *argv[]) {
       unsigned size_l1d_tag = 0;
       p_l1d_tag = read_tag(&size_l1d_tag, fp_l1d_tag);
       fclose(fp_l1d_tag);
-      for (unsigned i = 0; i < size_l1d_tag; i++) {
-         printf("l1d tag [%d]%lx\n", i, p_l1d_tag[i]);
+      if (size_l1d_tag == 0) {
+         printf("no valid addr in L1d!\n");
+         return 1;
       }
       // read l1i tag
       FILE *fp_l1i_tag;
@@ -766,15 +620,23 @@ int main(int argc, char *argv[]) {
       unsigned size_l1i_tag = 0;
       p_l1i_tag = read_tag(&size_l1i_tag, fp_l1i_tag);
       fclose(fp_l1i_tag);
-      for (unsigned i = 0; i < size_l1i_tag; i++) {
-         printf("l1i tag [%d]%lx\n", i, p_l1i_tag[i]);
+      if (size_l1i_tag == 0) {
+         printf("no valid addr in L1i!\n");
+         return 1;
       }
-      bdi_xor(line_array, lineSize, p, numLine, 
-         p_l1i_tag, p_l1d_tag, p_l2_tag);
+
+      init_line_data(line_array, lineSize, p, numLine);
+      // xor preprocess
+      xor_preprocess(line_array, lineSize, p, numLine, 
+         p_l1i_tag, size_l1i_tag, p_l1d_tag, size_l1d_tag, p_l2_tag,
+         do_xor);
+      // bdi compression
+      bdi(line_array, lineSize, p, numLine, do_xor);
    }
    else {
       // no need to read tag files
-      bdi(line_array, lineSize, p, numLine);
+      init_line_data(line_array, lineSize, p, numLine);
+      bdi(line_array, lineSize, p, numLine, do_xor);
    }
    
    // for every line:print
@@ -797,7 +659,7 @@ int main(int argc, char *argv[]) {
 
    // free up mem
    free(p);
-   if (do_xor == true) {
+   if (do_xor == 1 || do_xor == 2) {
       free(p_l2_tag);
       free(p_l1i_tag);
       free(p_l1d_tag);
